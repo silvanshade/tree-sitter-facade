c22c8a6cbb99ab6ac659eb57b870a5dcb16857f3
hvithrafn <hvithrafn@users.noreply.github.com>
2021-01-23 00:49:11 -0700

    Add Default impl and ::new for several types

--- src/input_edit.rs
+++ src/input_edit.rs
@@ -3,11 +3,38 @@ mod native {
     use crate::point::Point;
     use std::convert::TryFrom;
 
+    #[derive(Clone, Debug, Eq, PartialEq)]
     pub struct InputEdit {
         pub(crate) inner: tree_sitter::InputEdit,
     }
 
     impl InputEdit {
+        #[inline]
+        pub fn new(
+            start_byte: u32,
+            old_end_byte: u32,
+            new_end_byte: u32,
+            start_position: &Point,
+            old_end_position: &Point,
+            new_end_position: &Point,
+        ) -> Self {
+            let start_byte = start_byte as usize;
+            let old_end_byte = old_end_byte as usize;
+            let new_end_byte = new_end_byte as usize;
+            let start_position = start_position.inner;
+            let old_end_position = old_end_position.inner;
+            let new_end_position = new_end_position.inner;
+            tree_sitter::InputEdit {
+                start_byte,
+                old_end_byte,
+                new_end_byte,
+                start_position,
+                old_end_position,
+                new_end_position,
+            }
+            .into()
+        }
+
         #[inline]
         pub fn new_end_byte(&self) -> u32 {
             u32::try_from(self.inner.new_end_byte).unwrap()
@@ -42,6 +69,25 @@ mod native {
         }
     }
 
+    impl Default for InputEdit {
+        fn default() -> Self {
+            let start_index = Default::default();
+            let old_end_index = Default::default();
+            let new_end_index = Default::default();
+            let start_position = &Default::default();
+            let old_end_position = &Default::default();
+            let new_end_position = &Default::default();
+            Self::new(
+                start_index,
+                old_end_index,
+                new_end_index,
+                start_position,
+                old_end_position,
+                new_end_position,
+            )
+        }
+    }
+
     impl From<tree_sitter::InputEdit> for InputEdit {
         #[inline]
         fn from(inner: tree_sitter::InputEdit) -> Self {
@@ -57,11 +103,35 @@ pub use native::*;
 mod wasm {
     use crate::point::Point;
 
+    #[derive(Clone, Debug, Eq, PartialEq)]
     pub struct InputEdit {
         pub(crate) inner: web_tree_sitter::Edit,
     }
 
     impl InputEdit {
+        #[inline]
+        pub fn new(
+            start_byte: u32,
+            old_end_byte: u32,
+            new_end_byte: u32,
+            start_position: &Point,
+            old_end_position: &Point,
+            new_end_position: &Point,
+        ) -> Self {
+            let start_position = &start_position.inner;
+            let old_end_position = &old_end_position.inner;
+            let new_end_position = &new_end_position.inner;
+            web_tree_sitter::Edit::new(
+                start_byte,
+                old_end_byte,
+                new_end_byte,
+                start_position,
+                old_end_position,
+                new_end_position,
+            )
+            .into()
+        }
+
         #[inline]
         pub fn new_end_byte(&self) -> u32 {
             self.inner.new_end_index()
@@ -93,6 +163,25 @@ mod wasm {
         pub fn start_position(&self) -> Point {
             let inner = self.inner.start_position();
             Point { inner }
+        }
+    }
+
+    impl Default for InputEdit {
+        fn default() -> Self {
+            let start_index = Default::default();
+            let old_end_index = Default::default();
+            let new_end_index = Default::default();
+            let start_position = &Default::default();
+            let old_end_position = &Default::default();
+            let new_end_position = &Default::default();
+            Self::new(
+                start_index,
+                old_end_index,
+                new_end_index,
+                start_position,
+                old_end_position,
+                new_end_position,
+            )
         }
     }
 
