99baf30017b5c6eb1ad8643e66ca35a812a0fd67
silvanshade <silvanshade@users.noreply.github.com>
2021-01-25 20:06:19 -0700

    Formatting

--- src/error.rs
+++ src/error.rs
@@ -4,17 +4,16 @@ mod native {
     pub struct IncludedRangesError {
         pub(crate) inner: tree_sitter::IncludedRangesError,
     }
-    unsafe impl Send for IncludedRangesError {
-    }
-    unsafe impl Sync for IncludedRangesError {
-    }
+
     impl std::fmt::Display for IncludedRangesError {
         fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(fmt, "{:?}", self.inner)
         }
     }
+
     impl std::error::Error for IncludedRangesError {
     }
+
     impl From<tree_sitter::IncludedRangesError> for IncludedRangesError {
         #[inline]
         fn from(inner: tree_sitter::IncludedRangesError) -> Self {
@@ -22,21 +21,26 @@ mod native {
         }
     }
 
+    unsafe impl Send for IncludedRangesError {
+    }
+
+    unsafe impl Sync for IncludedRangesError {
+    }
+
     #[derive(Debug, Eq, PartialEq)]
     pub struct QueryError {
         pub(crate) inner: tree_sitter::QueryError,
     }
-    unsafe impl Send for QueryError {
-    }
-    unsafe impl Sync for QueryError {
-    }
+
     impl std::fmt::Display for QueryError {
         fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(fmt, "{:?}", self.inner)
         }
     }
+
     impl std::error::Error for QueryError {
     }
+
     impl From<tree_sitter::QueryError> for QueryError {
         #[inline]
         fn from(inner: tree_sitter::QueryError) -> Self {
@@ -44,21 +48,26 @@ mod native {
         }
     }
 
+    unsafe impl Send for QueryError {
+    }
+
+    unsafe impl Sync for QueryError {
+    }
+
     #[derive(Debug, Eq, PartialEq)]
     pub struct LanguageError {
         pub(crate) inner: tree_sitter::LanguageError,
     }
-    unsafe impl Send for LanguageError {
-    }
-    unsafe impl Sync for LanguageError {
-    }
+
     impl std::fmt::Display for LanguageError {
         fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             self.inner.fmt(fmt)
         }
     }
+
     impl std::error::Error for LanguageError {
     }
+
     impl From<tree_sitter::LanguageError> for LanguageError {
         #[inline]
         fn from(inner: tree_sitter::LanguageError) -> Self {
@@ -66,20 +75,30 @@ mod native {
         }
     }
 
+    unsafe impl Send for LanguageError {
+    }
+
+    unsafe impl Sync for LanguageError {
+    }
+
     #[derive(Debug, Eq, PartialEq)]
     pub struct ParserError {
         pub(crate) inner: std::convert::Infallible,
     }
-    unsafe impl Send for ParserError {
-    }
-    unsafe impl Sync for ParserError {
-    }
+
     impl std::fmt::Display for ParserError {
         fn fmt(&self, _fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             unreachable!()
         }
     }
+
     impl std::error::Error for ParserError {
+    }
+
+    unsafe impl Send for ParserError {
+    }
+
+    unsafe impl Sync for ParserError {
     }
 }
 
@@ -92,17 +111,22 @@ mod wasm {
     pub struct IncludedRangesError {
         pub(crate) inner: js_sys::Error,
     }
+
     unsafe impl Send for IncludedRangesError {
     }
+
     unsafe impl Sync for IncludedRangesError {
     }
+
     impl std::fmt::Display for IncludedRangesError {
         fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             fmt.write_str(&<_ as Into<String>>::into(self.inner.message()))
         }
     }
+
     impl std::error::Error for IncludedRangesError {
     }
+
     impl From<js_sys::Error> for IncludedRangesError {
         #[inline]
         fn from(inner: js_sys::Error) -> Self {
@@ -114,17 +138,22 @@ mod wasm {
     pub struct LanguageError {
         pub(crate) inner: web_tree_sitter::LanguageError,
     }
+
     unsafe impl Send for LanguageError {
     }
+
     unsafe impl Sync for LanguageError {
     }
+
     impl std::fmt::Display for LanguageError {
         fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             fmt.write_str(&<_ as Into<String>>::into(self.inner.message()))
         }
     }
+
     impl std::error::Error for LanguageError {
     }
+
     impl From<web_tree_sitter::LanguageError> for LanguageError {
         #[inline]
         fn from(inner: web_tree_sitter::LanguageError) -> Self {
@@ -136,17 +165,22 @@ mod wasm {
     pub struct ParserError {
         pub(crate) inner: web_tree_sitter::ParserError,
     }
+
     unsafe impl Send for ParserError {
     }
+
     unsafe impl Sync for ParserError {
     }
+
     impl std::fmt::Display for ParserError {
         fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             fmt.write_str(&<_ as Into<String>>::into(self.inner.message()))
         }
     }
+
     impl std::error::Error for ParserError {
     }
+
     impl From<web_tree_sitter::ParserError> for ParserError {
         #[inline]
         fn from(inner: web_tree_sitter::ParserError) -> Self {
@@ -158,17 +192,22 @@ mod wasm {
     pub struct QueryError {
         pub(crate) inner: web_tree_sitter::QueryError,
     }
+
     unsafe impl Send for QueryError {
     }
+
     unsafe impl Sync for QueryError {
     }
+
     impl std::fmt::Display for QueryError {
         fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             fmt.write_str(&<_ as Into<String>>::into(self.inner.message()))
         }
     }
+
     impl std::error::Error for QueryError {
     }
+
     impl From<web_tree_sitter::QueryError> for QueryError {
         #[inline]
         fn from(inner: web_tree_sitter::QueryError) -> Self {
